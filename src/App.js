ed665df66d414f0c7301ac96f926651d4ad50c1f
Your Namegit config --global user.name Your <you@example.com>
2022-06-22 15:39:05 -0400

    Close enough?

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import React from "react";
 import TopBar from "./components/TopBar";
 import BackImage from "./components/BackImage";
 import Stores from "./components/Stores";
